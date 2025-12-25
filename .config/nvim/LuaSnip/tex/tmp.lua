2f1d83e640cc252fa3268e2ff8553df1c2206e4a
hoshF <noveor.ol@gmail.com>
2025-12-25 22:41:24 +0800

    english preamble and nvim error? remove autopairs, change zathura to zathura-simple in vimtex

--- .config/nvim/LuaSnip/tex/tmp.lua
+++ .config/nvim/LuaSnip/tex/tmp.lua
@@ -1,25 +1,35 @@
-local helpers = require('luasnip-helper-funcs')
+local helpers = require("luasnip-helper-funcs")
 local get_visual = helpers.get_visual
 
 local line_begin = require("luasnip.extras.expand_conditions").line_begin
 
--- Math context detection 
+-- Math context detection
 local tex = {}
-tex.in_mathzone = function() return vim.fn['vimtex#syntax#in_mathzone']() == 1 end
-tex.in_text = function() return not tex.in_mathzone() end
-
+tex.in_mathzone = function()
+	return vim.fn["vimtex#syntax#in_mathzone"]() == 1
+end
+tex.in_text = function()
+	return not tex.in_mathzone()
+end
 
 return {
-  s({trig = "([^%a])MM", wordTrig = false, regTrig = true},
-    fmta(
-      "<>$<>$",
-      {
-        f( function(_, snip) return snip.captures[1] end ),
-        d(1, get_visual),
-      }
-    )
-  ),
+	s(
+		{ trig = "([^%a])MM", wordTrig = false, regTrig = true },
+		fmta("<>$<>$", {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			d(1, get_visual),
+		})
+	),
 
+	s(
+		{ trig = "([^%a])DM", wordTrig = false, regTrig = true },
+		fmta("<>\\[\n<>\n\\]", {
+			f(function(_, snip)
+				return snip.captures[1]
+			end),
+			d(1, get_visual),
+		})
+	),
 }
-
-
--- .config/nvim/lua/plugins/vimtex.lua
+++ .config/nvim/lua/plugins/vimtex.lua
@@ -2,7 +2,7 @@ return {
 	"lervag/vimtex",
 	ft = { "tex" },
 	init = function()
-		vim.g.vimtex_view_method = "zathura"
+		vim.g.vimtex_view_method = "zathura_simple"
 		vim.g.vimtex_compiler_method = "latexmk"
 		vim.g.vimtex_syntax_enabled = true
 		vim.g.vimtex_view_forward_search_on_start = 1
